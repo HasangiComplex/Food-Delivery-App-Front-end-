e51fb8bb8da16c1a43c80ec60d665cf9c08813d3
hansika_112197 <hansika.piumali@axiatadigitallabs.com>
2024-07-26 13:01:35 +0530

    completed Front of Food delivery website

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from "./pages/Cart/Cart"
-import PlaceOrder from "./pages/PlaceOrder/PlcaOrder"
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
 import Footer from './components/Footer/Footer'
 import LoginPopup from '././components/LoginPopup/LoginPopup'
 
@@ -23,7 +23,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/placeorder" element={<PlaceOrder />} />
+          <Route path="/order" element={<PlaceOrder />} />
         </Routes>
       </div>
       <Footer />
